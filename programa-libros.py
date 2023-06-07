11b45083b32454cc3cba6c36367e4d30d67a017b
Francaster <116579370+Francaster@users.noreply.github.com>
2023-06-07 10:54:46 -0300

    Update programa-libros.py

--- programa-libros.py
+++ programa-libros.py
@@ -9,6 +9,75 @@ cursor = conexion.cursor()
 # clase principal
 
 class Libreria:
+    def __init__(self):
+        self.conexion = sqlite3.connect('libreria.db')
+        self.cursor = self.conexion.cursor()
+        self.crear_tabla()
+
+    def crear_tabla(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS libros (
+                                id INTEGER PRIMARY KEY,
+                                isbn TEXT,
+                                titulo TEXT,
+                                autor TEXT,
+                                genero TEXT,
+                                precio REAL,
+                                cant_disponible INTEGER,
+                                fecha_registro TEXT)''')
+        self.conexion.commit()
+
+    def cargar_libro(self, id, isbn, titulo, autor, genero, precio, cant_disponible):
+        fecha_registro = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.cursor.execute('''INSERT INTO libros VALUES (?, ?, ?, ?, ?, ?, ?, ?)''',
+                            (id, isbn, titulo, autor, genero, precio, cant_disponible, fecha_registro))
+        self.conexion.commit()
+        print("Libro cargado exitosamente.")
+
+    def modificar_precio(self, id, nuevo_precio):
+        self.cursor.execute('''UPDATE libros SET precio = ? WHERE id = ?''', (nuevo_precio, id))
+        self.conexion.commit()
+        print("Precio del libro modificado exitosamente.")
+
+    def borrar_libro(self, id):
+        self.cursor.execute('''DELETE FROM libros WHERE id = ?''', (id,))
+        self.conexion.commit()
+        print("Libro borrado exitosamente.")
+
+    def cargar_disponibilidad(self, id, incremento):
+        self.cursor.execute('''SELECT cant_disponible FROM libros WHERE id = ?''', (id,))
+        cantidad_actual = self.cursor.fetchone()[0]
+        nueva_cantidad = cantidad_actual + incremento
+        self.cursor.execute('''UPDATE libros SET cant_disponible = ? WHERE id = ?''', (nueva_cantidad, id))
+        self.conexion.commit()
+        print("Disponibilidad cargada exitosamente.")
+
+    def listar_libros_ordenados(self):
+        self.cursor.execute('''SELECT * FROM libros ORDER BY titulo ASC''')
+        libros = self.cursor.fetchall()
+        for libro in libros:
+            print(libro)
+
+    def registrar_venta(self, libro_id, cantidad):
+        self.cursor.execute('''SELECT cant_disponible FROM libros WHERE id = ?''', (libro_id,))
+        cantidad_disponible = self.cursor.fetchone()[0]
+        if cantidad <= cantidad_disponible:
+            nueva_cantidad = cantidad_disponible - cantidad
+            self.cursor.execute('''UPDATE libros SET cant_disponible = ? WHERE id = ?''', (nueva_cantidad, libro_id))
+            self.conexion.commit()
+            print("Venta registrada exitosamente.")
+        else:
+            print("No hay suficiente cantidad disponible para realizar la venta.")
+
+    def actualizar_precios(self, porcentaje_aumento):
+        self.cursor.execute('''UPDATE libros SET precio = precio * (1 + ? / 100)''', (porcentaje_aumento,))
+        self.conexion.commit()
+        print("Precios actualizados exitosamente.")
+
+    def mostrar_registros_anteriores(self, fecha_limite):
+        self.cursor.execute('''SELECT * FROM libros WHERE fecha_registro < ?''', (fecha_limite,))
+        registros = self.cursor.fetchall()
+        for registro in registros:
+            print(registro)
     def ejecutar_menu(self):
         opcion = None
         while opcion != '0':
